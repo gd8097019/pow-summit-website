07fa046202fbc4cebf0f9169ba5bc74b6e46d656
gd8097019 <102132498+gd8097019@users.noreply.github.com>
2023-03-02 19:40:31 +0300

    Become speaker button url #15 (#20)
    * #6 fix circle problem in speakers
    
    * #4 Use LTC Foundation logo
    
    * #2 CSS updates for new figma design changes
    
    * #15 updates "Become a Speaker" button url

--- src/config/buttons.js
+++ src/config/buttons.js
@@ -12,7 +12,7 @@ const data = [
 	{
 		key: "become_speakers",
 		title: "Become a Speaker",
-		url: "https://www.google.com",
+		url: "https://docs.google.com/forms/d/1ZVeVLc1LCeqrr1tKl6-MdFtL5p-FHIeiDY5KL3EiiUs/edit",
 	},
 ];
 
