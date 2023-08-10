302b27eac58eccc6885434825871314a0cf94779
gd8097019 <102132498+gd8097019@users.noreply.github.com>
2023-08-10 23:04:58 +0300

    pow summit updates (#95)
    * media partners section
    
    * small update
    
    * add active sponsors
    
    * change position
    
    * change position sponsors and media sponsors section
    
    * test commit to rebuild

---
+++ src/components/ActiveSponsors/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<section class="activeSponsorsSection">
+		<h1>Sponsors</h1>
+		<div class="activeSponsorsContainer">
+			<a target="_blank" href="https://etccooperative.org/">
+				<img src="@/assets/images/etc-coop.png" alt="ETC Cooperative" />
+			</a>
+
+			<a target="_blank" href="https://lumerin.io/">
+				<img src="@/assets/images/lumerin.png" alt="Lumerin" />
+			</a>
+
+			<a target="_blank" href="https://hivedigitaltechnologies.com/">
+				<img
+					src="@/assets/images/hive-digital.png"
+					alt="HIVE Digital Technologies"
+				/>
+			</a>
+		</div>
+	</section>
+</template>
---
+++ src/components/MediaPartners/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<section class="mediaPartnersSection">
+		<h1>Media Sponsors</h1>
+		<div class="mediaPartnersContainer">
+			<a target="_blank" href="https://icoholder.com/">
+				<img src="@/assets/images/icoholder.png" alt="Icoholder" />
+			</a>
+			<a target="_blank" href="https://www.cryptonewsz.com/">
+				<img src="@/assets/images/cryptonewsz.png" alt="CryptoNewsZ" />
+			</a>
+			<a target="_blank" href="https://coinpedia.org/">
+				<img src="@/assets/images/coinpedia.png" alt="Coinpedia" />
+			</a>
+		</div>
+	</section>
+</template>
