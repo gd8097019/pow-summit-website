dc3e84de7eb3a2f937e7468ecb53910be5839b07
Andrew <andrewgdick@gmail.com>
2023-08-21 13:10:23 -0400

    Update index.vue (#110)

--- src/components/ActiveSponsors/index.vue
+++ src/components/ActiveSponsors/index.vue
@@ -5,6 +5,9 @@
 			<a target="_blank" href="https://etccooperative.org/">
 				<img src="@/assets/images/etc-coop.png" alt="ETC Cooperative" />
 			</a>
+            <a target="_blank" href="https://m.jasminer.com/">
+				<img src="@/assets/images/JASMINER_logo.png" alt="Jasminer" />
+			</a>
 
 			<a target="_blank" href="https://lumerin.io/">
 				<img src="@/assets/images/lumerin.png" alt="Lumerin" />
