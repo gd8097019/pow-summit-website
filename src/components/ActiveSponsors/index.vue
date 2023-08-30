292d847f71a0640694c2f67a9435aac920af2b56
gd8097019 <gd8097019@gmail.com>
2023-08-30 18:45:42 +0530

    Revised the Sponsors section

--- src/components/ActiveSponsors/index.vue
+++ src/components/ActiveSponsors/index.vue
@@ -2,23 +2,32 @@
 	<section class="activeSponsorsSection" id="activeSponsorsSection">
 		<h1>Sponsors</h1>
 		<div class="activeSponsorsContainer">
-			<a target="_blank" href="https://etcgrantsdao.io/">
-				<img src="@/assets/images/edg-logo-green-with-text.abb09187-2.png" alt="ETC Grants DAO" />
-			</a>
-            <a target="_blank" href="https://m.jasminer.com/">
-				<img src="@/assets/images/JASMINER_logo.png" alt="Jasminer" />
-			</a>
+			<div class="diamond-plus">
+				<h3>Diamond Plus</h3>
+				<a target="_blank" href="https://m.jasminer.com/">
+					<img src="@/assets/images/JASMINER_logo.png" alt="Jasminer" />
+				</a>
+			</div>
 
-			<a target="_blank" href="https://lumerin.io/">
-				<img src="@/assets/images/lumerin.png" alt="Lumerin" />
-			</a>
+			<div class="platinum">
+				<h3>Platinum</h3>
+				<div>
+					<a target="_blank" href="https://etccooperative.org/">
+						<img src="@/assets/images/etc-coop.png" alt="ETC Cooperative" />
+					</a>
 
-			<a target="_blank" href="https://hivedigitaltechnologies.com/">
-				<img
-					src="@/assets/images/hive-digital.png"
-					alt="HIVE Digital Technologies"
-				/>
-			</a>
+					<a target="_blank" href="https://hivedigitaltechnologies.com/">
+						<img src="@/assets/images/hive-digital.png" alt="HIVE Digital Technologies" />
+					</a>
+				</div>
+			</div>
+
+			<div class="silver">
+				<h3>Silver</h3>
+				<a target="_blank" href="https://lumerin.io/">
+					<img src="@/assets/images/lumerin.png" alt="Lumerin" />
+				</a>
+			</div>
 		</div>
 	</section>
 </template>
