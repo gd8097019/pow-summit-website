f35eb743d7b80dc709f240d5a37eb0939f65c05d
gd8097019 <102132498+gd8097019@users.noreply.github.com>
2023-09-11 23:43:50 +0530

    Added Sponsors and removed Early Bird button (#166)
    * Removed Early Bird discount button
    
    * Added Lokotech & iPollo Sponsors

--- src/components/ActiveSponsors/index.vue
+++ src/components/ActiveSponsors/index.vue
@@ -4,9 +4,14 @@
 		<div class="activeSponsorsContainer">
 			<div class="diamond-plus">
 				<h3>Diamond Plus</h3>
-				<a target="_blank" href="https://m.jasminer.com/">
-					<img src="@/assets/images/JASMINER_logo.png" alt="Jasminer" />
-				</a>
+				<div>
+					<a target="_blank" href="https://m.jasminer.com/">
+						<img src="@/assets/images/JASMINER_logo.png" alt="Jasminer" />
+					</a>
+					<a target="_blank" href="https://www.lokotech.no/">
+						<img src="@/assets/images/lokotech.png" alt="Lokotech" />
+					</a>
+				</div>
 			</div>
 
 			<div class="platinum">
@@ -24,9 +29,14 @@
 
 			<div class="silver">
 				<h3>Silver</h3>
-				<a target="_blank" href="https://lumerin.io/">
-					<img src="@/assets/images/lumerin.png" alt="Lumerin" />
-				</a>
+				<div>
+					<a target="_blank" href="https://lumerin.io/">
+						<img src="@/assets/images/lumerin.png" alt="Lumerin" />
+					</a>
+					<a target="_blank" href="https://ipollo.com/">
+						<img src="@/assets/images/ipollo.png" alt="iPollo" />
+					</a>
+				</div>
 			</div>
 		</div>
 	</section>
