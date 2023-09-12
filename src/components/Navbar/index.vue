2ff313c26225680946214066821199c64e7827c0
gd8097019 <102132498+gd8097019@users.noreply.github.com>
2023-09-12 23:43:20 +0530

    Revised the Agenda link (#176)

--- src/components/Navbar/index.vue
+++ src/components/Navbar/index.vue
@@ -33,7 +33,7 @@
 							<a class="nav-link" href="#ourSpeakersSection">Speakers</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#agendaSection">Agenda</a>
+							<a class="nav-link" href="#agenda">Agenda</a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" href="#getTicketSection">Tickets</a>
