3a74162c035209ba8d8d76fbfa125498036391e3
Andrew <andrewgdick@gmail.com>
2023-11-01 14:45:30 -0400

    Update index.vue (#216)
    updated copy & images for header

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -12,9 +12,9 @@
 		<div class="proofHeroHead">
 			<h2>SECURING BLOCKCHAIN'S FUTURE</h2>
 			<p>
-				A cypherpunk-grounded conference which refutes misleading
-				mainstream narratives around proof-of-work and focuses on
-				the fundamentals of this revolutionary freedom technology.
+Our cypherpunk-grounded, proof-of-work summit in Prague this fall was a smashing success. 
+Our speakers and guests discussed Proof of Work and pushed back against the misleading mainstream narratives 
+around PoW and focused on the fundamentals of this revolutionary freedom technology.
 			</p>
 		</div>
 
@@ -22,12 +22,12 @@
 			<div class="col-lg-4 col-md-4 col-sm-6 col-12 px-md-3">
 				<div class="heroCard">
 					<div class="heroCardImg">
-						<img src="@/assets/images/heroImg1.jpg" alt="" />
+						<img src="@/assets/images/heroImg1.png" alt="" />
 					</div>
 					<div class="heroCardContent">
 						<h3>WORLD CLASS <span>SPEAKERS</span></h3>
 						<p>
-							Hear from true builders, visionaries, rebels, troublemakers and dreamers.
+							Watch our video archive on our<a href="https://www.youtube.com/@POWSummit/"> YouTube channel</a> featuring Phil Zimmermann, Jameson Lopp, Charlie Lee, Caitlin Long & Jared Hope & more...
 						</p>
 					</div>
 				</div>
@@ -35,12 +35,12 @@
 			<div class="col-lg-4 col-md-4 col-sm-6 col-12 px-md-3">
 				<div class="heroCard">
 					<div class="heroCardImg">
-						<img src="@/assets/images/ltc-summit.af786470.png" alt="" />
+						<img src="@/assets/images/heroImg2.png" alt="" />
 					</div>
 					<div class="heroCardContent">
 						<h3>NETWORKING <span>OPPORTUNITIES</span></h3>
 						<p>
-							An amazing opportunity to meet experts from around the world face-to-face.
+							Meet & interact with speakers & guests at the conference or at our after-hours events.  <a href="https://powsummit.com/photos">See our photos here.</a> 
 						</p>
 					</div>
 				</div>
@@ -53,7 +53,7 @@
 					<div class="heroCardContent">
 						<h3>HANDS-ON <span>WORKSHOPS</span></h3>
 						<p>
-							Learn directly from the builders themselves.
+							Learn & interact with the builders & developers themselves. Our <a href="https://www.youtube.com/playlist?list=PL6M4J55WQYzwnsF9O3NO8ujBcm_uavhP5">2023 workshops</a> are also available on our YouTube channel. 
 						</p>
 					</div>
 				</div>
