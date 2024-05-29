2331786bc632916b440571bd09fbe111e9d7c044
Andrew <andrewgdick@gmail.com>
2024-05-29 10:08:25 -0400

    Added Accommodation section / Changed Name of Social Image (#255)
    * hotel image
    
    hotel image
    
    * Update index.vue Adding Accommodation to Location
    
    Adding Accommodation to Location
    
    * Changing up Social.png 
    
    Changing social png
    
    * Update index.html social-2024.png
    
    Switch metadata to social-2024.png to refresh website sharing preview

--- src/components/2024/Location/index.vue
+++ src/components/2024/Location/index.vue
@@ -22,5 +22,30 @@
 			</div>
 		</div>
 	</section>
+	<section class="whereToStaySection">
+		<div class="proofMainContainer whereToStayContainer">
+			<div class="row">
+				<div class="col-lg-6 pe-lg-4 pe-3 order-lg-1 order-2">
+					<img src="@/assets/images/holidayinn.png" alt="" />
+				</div>
+				<div class="col-lg-6 mb-lg-0 mb-5 order-lg-2 order-1 text-center">
+					<h1>ACCOMMODATIONS</h1>
+					<p>
+						Our recommended hotel is the Holiday Inn Alte Oper which is a 4 minute drive (or 11
+minute walk) from the Stages venue.
+					</p>
+                   <p> We have secured a special rate for all POW Summit attendees. Rooms are expected to fill up fast due to
+Oktoberfest, so book your ticket now!</p>
+					<p>Mainzer Landstraße 27, 60329 Frankfurt am Main, Germany</p>
+					<a
+						href="https://www.ihg.com/holidayinn/hotels/us/en/find-hotels/select-roomrate?fromRedirect=true&qSrt=sBR&qIta=99801505&icdv=99801505&qSlH=FRAAO&qCiD=27&qCiMy=082024&qCoD=30&qCoMy=082024&qGrpCd=MTG&setPMCookies=true&qSHBrC=HI&qDest=Mainzer%20Landstrasse%2027%2C%20Frankfurt%2C%20DE&srb_u=1"
+						class="text-decoration-none"
+						target="_blank"
+						><button class="bookNowBtn">BOOK NOW</button></a
+					>
+				</div>
+			</div>
+		</div>
+	</section>
 
 </template>
